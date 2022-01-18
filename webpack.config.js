5cd61e99501d8c65b8b5992925d31ea17f87c767
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-01-18 20:03:31 +0200

    Update quotes and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: './build/index.js',
-    target: 'web',
+    mode: "production",
+    entry: "./build/index.js",
+    devtool: "source-map",
+    target: "web",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'frontendtools.min.js'
+        path: path.resolve(__dirname, "dist"),
+        filename: "frontendtools.min.js"
     },
     resolve: {
-        modules: ['./node_modules']
+        modules: ["./node_modules"]
     }
 }

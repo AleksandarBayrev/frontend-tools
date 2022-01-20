c400409945ff58400f874fcff1b03167c43975ec
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-01-20 15:17:18 +0200

    Fix debounce implementation

--- src/utils/internal/debounce/debounce.test.ts
+++ src/utils/internal/debounce/debounce.test.ts
@@ -1,27 +1,44 @@
 import { debounce } from "./debounce";
 
 describe("debounce", () => {
+    let mapDelete = jest.spyOn(Map.prototype, 'delete');
     beforeAll(() => {
         jest.useFakeTimers();
     });
+
+    afterEach(() => {
+        mapDelete = mapDelete.mockReset();
+    });
+
     afterAll(() => {
         jest.useRealTimers();
     });
     it("calls function after a specified amount of time", () => {
-        const callback = jest.fn((name, age) => {});
+        const firstCallback = jest.fn((name, age) => {});
         const params = ['test', 1];
         const timeout = 100;
-        debounce(callback, null, params, timeout);
+        debounce(firstCallback, null, params, timeout);
         jest.advanceTimersByTime(100);
-        expect(callback).toHaveBeenCalledTimes(1);
-        expect(callback).toHaveBeenCalledWith(params[0], params[1]);
+        expect(firstCallback).toHaveBeenCalledTimes(1);
+        expect(firstCallback).toHaveBeenCalledWith(params[0], params[1]);
     });
     it("doesn't call function before the specified amount of time", () => {
-        const callback = jest.fn((name, age) => {});
+        const secondCallback = jest.fn((name, age) => {});
         const params = ['test', 1];
         const timeout = 100;
-        debounce(callback, null, params, timeout);
+        debounce(secondCallback, null, params, timeout);
         jest.advanceTimersByTime(99);
-        expect(callback).toHaveBeenCalledTimes(0);
+        expect(secondCallback).toHaveBeenCalledTimes(0);
+    });
+    it("calls function after a specified amount of time and deletes previously stored timer reference", () => {
+        const thirdCallback = jest.fn((name, age) => {});
+        const params = ['test', 1];
+        const timeout = 100;
+        debounce(thirdCallback, null, params, timeout);
+        debounce(thirdCallback, null, params, timeout);
+        jest.advanceTimersByTime(100);
+        expect(thirdCallback).toHaveBeenCalledTimes(1);
+        expect(mapDelete).toHaveBeenCalledTimes(3);
+        expect(thirdCallback).toHaveBeenCalledWith(params[0], params[1]);
     });
 });
--- src/utils/internal/debounce/debounce.ts
+++ src/utils/internal/debounce/debounce.ts
@@ -1,14 +1,21 @@
 import { Debounce } from "./debounce.types";
 
 const timeouts: Map<string, any> = new Map();
+
+const resetTimeout = (timeouts: Map<string, any>, callback: Function) => {
+    clearTimeout(timeouts.get(callback.name));
+    timeouts.delete(callback.name);
+}
+
 export const debounce: Debounce = (callback: Function, thisArg: any, parameters: any[], timeout: number) => {
-    if (!timeouts.has(callback.name)) {
-        timeouts.set(callback.name,
-            setTimeout(() => {
-                callback.apply(thisArg, parameters);
-                clearTimeout(timeouts.get(callback.name));
-                timeouts.delete(callback.name);
-            }, timeout)
-        );
+    if (timeouts.has(callback.name)) {
+        resetTimeout(timeouts, callback);
     }
+
+    timeouts.set(callback.name,
+        setTimeout(() => {
+            callback.apply(thisArg, parameters);
+            resetTimeout(timeouts, callback);
+        }, timeout)
+    );
 }
